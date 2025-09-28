a084827eae8ac52dd5bda0fcb6e6571bf80ceb11
Fahad Hussain Zardari <fahadzardari111@gmail.com>
2025-09-28 08:58:42 +0500

    feat: Add Accordion and SwiperSection components to Web Development page with dynamic content

---
+++ src/data/swipers.js
@@ -0,0 +1,42 @@
+export const webDevSwiperCards = [
+  {
+    "serialNo": "01",
+    "title": "Initial Consultation",
+    "description": "We begin by gaining a deep understanding of your brand, goals, and target audience. This collaboration ensures that we align your vision with our creative direction, setting the foundation for a truly bespoke website.",
+    "textColor": "text-[#2A2111]",
+    "serialBg": "bg-[#96A2FF]",
+    "background": "bg-[#E0E4FF]"
+  },
+  {
+    "serialNo": "02",
+    "title": "Strategy & Planning",
+    "description": "Every project starts with a meticulously crafted strategy, where we outline everything from website functionality to design concepts. This phase ensures that every aspect of your website is aligned with your business objectives.",
+    "textColor": "text-[#2A2111]",
+    "serialBg": "bg-[#1DBBCB]",
+    "background": "bg-[#DEFCFF]"
+  },
+  {
+    "serialNo": "03",
+    "title": "Design & Development",
+    "description": "Our design team creates a polished, high-quality design, while our developers bring it to life with cutting-edge technology. Rigorous testing ensures that the website is not only beautiful but also seamlessly functional across all devices.",
+    "textColor": "text-[#FFFFFF]",
+    "serialBg": "bg-[#3D3D3D]",
+    "background": "bg-[#000000]"
+  },
+  {
+    "serialNo": "04",
+    "title": "Launch Preparation",
+    "description": "Before launching, we optimize your website for speed, security, and SEO, ensuring flawless performance. The final touches are made to guarantee a seamless experience when the website goes live.",
+    "textColor": "text-[#2A2111]",
+    "serialBg": "bg-[#96A2FF]",
+    "background": "bg-[#E0E4FF]"
+  },
+  {
+    "serialNo": "05",
+    "title": "Post-Launch Support",
+    "description": "Our commitment to your success doesn’t stop at launch. We provide continuous support and maintenance, ensuring your website evolves and stays ahead in an ever-changing digital landscape.",
+    "textColor": "text-[#2A2111]",
+    "serialBg": "bg-[#1DBBCB]",
+    "background": "bg-[#DEFCFF]"
+  }
+];
