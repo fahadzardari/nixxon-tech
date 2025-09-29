60a080710aaaaf58af5708fedc3c7e3325f2b164
Fahad Hussain Zardari <fahadzardari111@gmail.com>
2025-09-29 16:04:04 +0500

    feat: Add new components for About and Branding sections, including images and animations

--- src/utils/gsapUtils.js
+++ src/utils/gsapUtils.js
@@ -7,19 +7,3 @@ gsap.registerPlugin(ScrollTrigger);
 // Export configured instance
 export { gsap, ScrollTrigger };
 
-// // Utility function to create fade-up animations
-// export const createFadeUpAnimation = (elements, options = {}) => {
-//   const defaultOptions = {
-//     y: 50,
-//     opacity: 0,
-//     duration: 0.8,
-//     ease: "power2.out",
-//     stagger: 0.15,
-//     scrollTrigger: {
-//       trigger: elements[0].parentElement || elements[0],
-//       start: "top 80%",
-//     }
-//   };
-
-//   return gsap.from(elements, {...defaultOptions, ...options});
-// };
